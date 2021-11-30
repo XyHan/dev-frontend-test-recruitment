799e4f07b73c51c34139dd02623872079c67070d
Robin <chevalier.robin@yahoo.fr>
2021-11-30 19:57:20 +0100

    refacto(component): remove useless theme var from menu component

--- app/src/ui/components/menu/menu.component.ts
+++ app/src/ui/components/menu/menu.component.ts
@@ -1,28 +1,8 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { ThemeManagerInterface, ThemeType } from '../../../domain/manager/theme.manager.interface';
-import { ThemeManager } from '../../../infrastructure/app/manager/theme.manager';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
-  private readonly _themeManager: ThemeManagerInterface;
-  private _currentTheme: ThemeType | null;
-
-  constructor(@Inject(ThemeManager) themeManager: ThemeManagerInterface) {
-    this._themeManager = themeManager;
-    this._currentTheme = 'light';
-  }
-
-  ngOnInit(): void {
-    this._themeManager.theme.subscribe(
-      (theme: ThemeType) => this._currentTheme = theme
-    );
-  }
-
-  get currentTheme(): string | null {
-    return this._currentTheme;
-  }
-}
+export class MenuComponent {}
