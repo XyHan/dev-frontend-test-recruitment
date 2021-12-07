bd4a6b9c213d8ddf8797ce900eb86bf54310d8e6
Robin <xyhan_1@hotmail.com>
2021-12-07 20:20:36 +0100

    clean(): remove useless entity

--- app/src/infrastructure/music/entity/track/artwork.entity.ts
+++
@@ -1,7 +0,0 @@
-import { ArtworkModel } from '../../../../domain/music/model/track/artwork.model';
-import { Expose } from 'class-transformer';
-
-export class ArtworkEntity extends ArtworkModel {
-  @Expose({ name: 'media' })
-  override '150x150': string;
-}
